d3329e5961051d3368e3ce236225133f58a296c7
Jennifer Wang 🥰 <jenniferwang@2020-jenns-macbook-pro.lan>
2024-10-04 18:50:03 -0700

    added new stuff

---
+++ ipynb Jupyter Notebook Homework/homework1 answers.py
@@ -0,0 +1,18 @@
+## homework1 answers
+
+'''
+I was late so didn't get scored for this homework.
+'''
+
+2) How many records are in the dataset? 2160 
+
+3) How many brands? 27
+
+4) Max price for DELL NOTEBOOK: 3936
+
+#6) Find median and mode. Has it changed for this dataset? 
+No
+
+#7) Sum of weights (all elements in the results) 
+
+91.30 
